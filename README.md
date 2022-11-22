7798020542488170138b6e44b98a5f8a5021cece
Kirill-Kukuliev <keha12345@gmail.com>
2022-11-22 18:56:24 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,98 @@
-# fs-router
+# fs_router
 it is a router from koa and express servers with suport of roots from fs path
+# [fs_router](https://github.com/keha12345/fs-router)
+
+> Router middleware for [Koa](https://github.com/koajs/koa). Maintained by [Forward Email][forward-email] and [Lad][].
+[![license](https://img.shields.io/github/license/koajs/router.svg)](LICENSE)
+
+
+## Table of Contents
+
+* [Features](#features)
+* [Install](#install)
+* [Description](#description)
+* [Exemples](#exemples)
+* [Future](#future)
+* [License](#license)
+
+
+## Features
+
+* Automatic root generation
+* Protected router from an accidental call
+* `async/await` support
+
+## Install
+
+[npm][]:
+
+```sh
+npm install fs_router
+```
+
+
+## Description
+
+You can get automatic root controllers in the file system
+
+```
+routers
+    foo
+        bar.js
+        baz.js
+    other.js
+```
+you will get root:
+/foo/bar
+/foo/baz
+/other
+
+
+## Exemples
+
+### for a Koa
+```index.js
+const Koa = require("koa");
+const cors = require("@koa/cors");
+const router = require('fs_router').koa;
+const app = new Koa();
+
+app.use(parser())
+  .use(cors())
+  .use(router('routers',__dirname))
+  .listen(8000, () => {
+    console.log(`🚀 on port:${8000}`);
+  });
+```
+
+```controller.js
+const Router = require('fs_router').Router
+
+module.exports = new Router({
+        get: (cxt) => {return `hello ${cxt.method}`},
+        post: async (cxt) => {
+            await cxt.db.model.create(object)
+            return 'ok'
+        }
+    },
+    (cxt) => { if(!cxt.session) throw Error('auth failed')}
+)
+```
+
+
+## Future
+
+| Name             |
+| ---------------- |
+| **Alex Mingoia** |
+| **@koajs**       |
+
+
+## License
+
+[MIT](LICENSE) © Kirill Kukuliev
+
+
+##
+
+Thank a million!
---
+++ index.js
@@ -0,0 +1,124 @@
+const fs = require('fs');
+const p = require('path');
+
+module.exports.Router = class Router{
+    /**
+     * 
+     * @param {{method: Function,}} controllers - example { get: (cxt) => {...} }
+     * @param {Function} checkCb - middleware, calls to next() automaticly, you should to throw if resurse forbidden
+     */
+    constructor(controllers, checkCb = null){
+        this.controllersObj = controllers;
+        this.checkCb = checkCb;
+    }
+    _getCbs(){
+        return Object.keys(this.controllersObj)
+        .reduce((acc, el) => ({
+            ...acc, 
+            [el]: (cxt)=>{
+                try {
+                    if(typeof this.checkCb === 'function') this.checkCb(cxt);
+                } catch (error) {
+                    throw Error('forbidden');
+                }
+                return this.controllersObj[el](cxt);
+        }}),{})
+    }
+}
+
+/**
+ * 
+ * @param {String} path - path to folder with controllers
+ * @param {String} dirname - it is __dirname 
+ * @returns {Function} middleware callback from koa app
+ */
+module.exports.koa = function(path,dirname){
+    const routes = getRoutes(path,dirname);
+    return async  (cxt, next) => {
+        const requirePath = p.join(dirname,routes[cxt.request.url.split('?')[0]]);
+        if(requirePath) {
+            const router = require(requirePath);
+            if(router instanceof module.exports.Router) {
+                const cb = router._getCbs()[cxt.request.method.toLowerCase()]
+                if(cb && typeof cb === 'function'){
+                    try {
+                        let resp = cb(cxt, next)
+                        if(resp instanceof Promise) resp = await resp;
+                        if (resp instanceof Object) cxt.body = JSON.parse(resp);
+                        else cxt.body = resp;
+                    } catch (error) {
+                        if(error?.message?.includes('forbidden')){
+                            cxt.body = '...oooups! resourse forbidden!'
+                            cxt.status = 403;
+                        }
+                        else throw Error(`${requirePath}
+                        Method ${cxt.request.method.toLowerCase()} has an error: ${error.message} \n`);
+                    }
+                    return;
+                }
+            }
+        }
+        cxt.body = '...oooups! resourse not found!'
+        cxt.status = 404;
+    }
+}
+
+/**
+ * 
+ * @param {String} path - path to folder with controllers
+ * @param {String} dirname - it is __dirname 
+ * @returns {Function} middleware callback from express app
+ */
+module.exports.express = function(path,dirname){
+    const routes = getRoutes(path,dirname);
+    return async  (req, res, next) => {
+        const requirePath = p.join(dirname,routes[req.path]);
+        if(requirePath) {
+            const router = require(requirePath);
+            if(router instanceof module.exports.Router) {
+                const cb = router._getCbs()[req.method.toLowerCase()]
+                if(cb && typeof cb === 'function'){
+                    try {
+                        let resp = cb(req, res, next)
+                        if(resp instanceof Promise) resp = await resp;
+                        if (resp instanceof Object) res.json(resp);
+                        else res.send(resp);
+                    } catch (error) {
+                        if(error?.message?.includes('forbidden')){
+                            res.status(403).send('...oooups! resourse forbidden!');
+                        }
+                        else throw Error(`${requirePath}
+                        Method ${req.method.toLowerCase()} has an error: ${error.message} \n`);
+                    }
+                    return;
+                }
+            }
+        }
+        res.status(404).send('...oooups! resourse not found!');
+    }
+}
+
+function getRoutes(link,dirname){
+   try {
+        return getRoutesObj(link,dirname)
+        .reduce((acc, el)=>({
+            ...acc,
+            [el.split(link.replace('./',''))[1].replace('.js','')]: './'+el
+        }),{})
+   } catch (error) {
+        throw Error(`Incorrect path "${link}"\n or dirname "${dirname}" \n`)
+   }
+}
+
+function getRoutesObj(link,dirname){
+    const routes= [];
+    for(let el of fs.readdirSync(p.join(dirname,link))){
+        try {
+            routes.push(...routes,...getRoutesObj(p.join(link,el),dirname));
+        } catch {
+            routes.push(...routes, p.join(link,el));
+        }
+    }
+    return routes
+}
+
